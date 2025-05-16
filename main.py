cbb37adea237f3c5bb4d7a24026ae07db5752b18
Ioannis Toumpoglou <ioannis.toumpoglou@gmail.com>
2025-05-16 10:11:38 +0300

    Implement PUT request endpoint to update ToDo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from typing import Annotated
-
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException, Path
@@ -40,13 +39,32 @@ async def get_all_todos(db: db_dependency):
 @app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
 async def get_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+
     if todo_model is not None:
         return todo_model
+
     raise HTTPException(status_code=404, detail="Todo not found")
 
 
 @app.post("/todo", status_code=status.HTTP_201_CREATED)
 async def create_todo(db: db_dependency, todo_request: TodoRequest):
-    todo_model = Todos(**todo_request.dict())
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
+
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.complete = todo_request.complete
+
     db.add(todo_model)
     db.commit()
