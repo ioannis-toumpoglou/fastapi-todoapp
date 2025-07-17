1cf39bf461078ce57e91f958a0a1404a93f032b9
Ioannis Toumpoglou <ioannis.toumpoglou@gmail.com>
2025-07-17 13:51:54 +0300

    Add router for authentication

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ import models
 from models import Todos
 from database import engine, SessionLocal
 
+from routers import auth
+
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
 
 
 def get_db():
